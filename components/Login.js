b0f3a9e6003dce4af581ac6443aaf741fd5b7b32
Dhruv G Pai Dukle <Dhruvdukle@yahoo.com>
2021-12-18 10:22:13 +0530

    first commit

---
+++ components/Login.js
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
+
+function Login() {
+
+    const { authenticate } = useMoralis();
+
+    return (
+
+        <div className="bg-black relative">
+
+            <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+
+                {/* logo image */}
+                <Image 
+                    src="https://g.foolcdn.com/image/?url=https%3A//g.foolcdn.com/editorial/images/655516/metaverse-vr-headset.jpg&w=1000&op=resize"
+                    height={200}
+                    width={200}
+                    className="object-cover rounded-full"
+                />
+
+                {/* login button */}
+                <button 
+                    className="bg-[#296E86] rounded-lg p-5 font-bold animate-pulse"
+                    onClick={authenticate}
+                > 
+                    Login to the METAVERSE 
+                </button>
+
+            </div>
+
+            <div className="w-full h-screen">
+                <Image 
+                    src="https://links.papareact.com/55n"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Login;
